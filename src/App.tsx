001a1e19e7e4efdbb35e470836cbaaa8b4f7bada
ruru <ruru.dev07@gmail.com>
2026-01-03 12:33:34 +0530

    feat: minor update stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 import { check } from "@tauri-apps/plugin-updater";
 
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [update, setUpdate] = useState<string | null>(null);
+  
   return (
     <main>
       <button
         onClick={async () => {
-          const data = await check();
-          console.log(data);
+          const update = await check();
+
+          if (update) {
+            setUpdate(`found update ${update.version} from ${update.date} with notes ${update.body} - current version is ${update.currentVersion}`);
+          }
         }}
       >
         check for updates
       </button>
+      {update && <p>{update}</p>}
     </main>
   );
 }
